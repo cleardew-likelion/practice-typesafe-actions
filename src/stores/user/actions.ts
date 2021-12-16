dadb7ccf42514e2ec81fc56fd676ca77209ac8c8
irisdew <cleardew@likelion.net>
2021-12-16 15:25:43 +0900

    :sparkles: ADD: user types, actions

---
+++ src/stores/user/actions.ts
@@ -0,0 +1,9 @@
+import { createAsyncAction } from "typesafe-actions";
+import { AxiosError } from "axios";
+import { IUserList } from "./types";
+
+export const fetchUserList = createAsyncAction(
+  "user/FETCH_USER_LIST",
+  "user/FETCH_USER_LIST_SUCCESS",
+  "user/FETCH_USER_LIST_FAIL"
+)<unknown, IUserList, AxiosError<any, any>>();
---
+++ src/stores/user/types.ts
@@ -0,0 +1,24 @@
+import { ActionType } from "typesafe-actions";
+import * as actions from "./actions";
+
+export type IUserAction = ActionType<typeof actions>;
+
+export interface IUser {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
+export interface IUserList {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  data: IUser[];
+  support: {
+    url: string;
+    text: string;
+  };
+}
